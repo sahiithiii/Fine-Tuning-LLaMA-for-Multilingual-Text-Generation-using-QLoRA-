91baefcab3d83ff23fcc49dc2de833546889d4e4
Sahithi Akula <sahithi.akulaa@gmail.com>
2026-07-27 13:47:30 +0530

    model training

---
+++ train.py
@@ -0,0 +1,117 @@
+from datasets import load_dataset
+from colorama import Fore,init
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+import torch
+from multiprocessing import freeze_support
+from trl import SFTTrainer,SFTConfig
+from peft import LoraConfig,prepare_model_for_kbit_training
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+HUGGING_FACE_TOKEN = os.getenv("HUGGING_FACE_TOKEN")
+
+if HUGGING_FACE_TOKEN is None:
+    raise ValueError("HF_TOKEN not found in .env file")
+
+init(autoreset=True)
+def main():
+    dataset = load_dataset(
+        "json",
+        data_files="data/instruction.jsonl",
+        split="train"
+    )
+    print(Fore.YELLOW+str(dataset[2]))
+
+    def format_chat_template(batch,tokenizer):
+        system_prompt="""You are a helpful, honest and harmless assistant designed to help engineers.
+        Think through each question logically and provide an answer. Don't make things up, if you're
+        unable to answer a question, advise the  user that you're unable to asnwer as it is outside 
+        of your scope."""
+        
+        # Apply chat template and append the result to the list
+        tokenizer.chat_template = "{% set loop_messages = messages %}{% for message in loop_messages %}" \
+        "{% set content = '<|start_header_id|>' + message['role'] + '<|end_header_id|>\n\n'+ message['content'] " \
+        "| trim + '<|eot_id|>' %}{% if loop.index0 == 0 %}{% set content = bos_token + content %}{% endif %}{{ content }}" \
+        "{% endfor %}{% if add_generation_prompt %}{{ '<|start_header_id|>assistant<|end_header_id|>\n\n' }}{% endif %}"
+
+        samples=[]
+        questions=batch["question"]
+        answers=batch["answer"]
+        for i in range(len(questions)):
+            row_json=[
+                {"role":"system","content":system_prompt},
+                {"role":"user","content":questions[i]},
+                {"role":"assistant","content":answers[i]}
+            ]
+
+            text=tokenizer.apply_chat_template(row_json,tokenize=False)
+            samples.append(text)
+
+        return {
+                "instruction":questions,
+                "response":answers,
+                "text":samples
+        }
+
+    base_model="meta-llama/Llama-3.2-3B-Instruct"  #Llama-3.2-1B
+    tokenizer=AutoTokenizer.from_pretrained(
+        base_model,
+        trust_remote_code=True,
+        token=HUGGING_FACE_TOKEN
+    )
+    def preprocess(batch):
+        return format_chat_template(batch, tokenizer)
+
+    train_dataset=dataset.map(
+        preprocess,
+        batched=True,
+        batch_size=10
+    )
+    print(Fore.LIGHTMAGENTA_EX+str(train_dataset[0]))
+
+    quant_config=BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.bfloat16,
+    )
+
+    model=AutoModelForCausalLM.from_pretrained(
+        base_model,
+        device_map="auto",
+        quantization_config=quant_config,
+        token=HUGGING_FACE_TOKEN,
+        cache_dir="./workspace",
+    )
+
+    print(Fore.CYAN+str(model))
+    print(Fore.LIGHTYELLOW_EX+str(next(model.parameters())))
+
+    model.gradient_checkpointing_enable()
+    model=prepare_model_for_kbit_training(model)
+
+    peft_config=LoraConfig(
+        r=32,
+        lora_alpha=64,
+        lora_dropout=0.05,
+        target_modules="all-linear",
+        task_type="CAUSAL_LM"
+    )
+
+    trainer=SFTTrainer(
+        model,
+        train_dataset=train_dataset,
+        args=SFTConfig(output_dir="meta-llama/Llama-3.2-1B-SFT",num_train_epochs=50),
+        peft_config=peft_config,
+    )
+
+    trainer.train()
+
+    trainer.save_model('complete_checkpoint')
+    trainer.model.save_pretrained("final_model")
+
+if __name__ == "__main__":
+    freeze_support()
+    main()
